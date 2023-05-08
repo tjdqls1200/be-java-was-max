3e9d0886c137faea4e758fe50af461a7af362636
code-tree <dltjdqls1200@naver.com>
2023-05-08 16:18:06 +0900

    refactor: WebServer 기본 코드 변경
    - 스레드풀 사용

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,33 +1,40 @@
 package webserver;
 
 import java.net.ServerSocket;
-import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WebServer {
-    private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
 
-    public static void main(String args[]) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
-        }
+    public static void main(String[] args) throws Exception {
+        final ExecutorService executorService = Executors.newFixedThreadPool(10);
+        final int port = getPort(args);
 
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
-        try (ServerSocket listenSocket = new ServerSocket(port)) {
-            logger.info("Web Application Server started {} port.", port);
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            LOGGER.info("Web Application Server started {} port.", port);
 
-            // 클라이언트가 연결될때까지 대기한다.
-            Socket connection;
-            while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+            while (isConnected(serverSocket)) {
+                executorService.execute(new RequestHandler(serverSocket.accept()));
             }
         }
+
+        executorService.shutdown();
+    }
+
+    private static int getPort(String[] args) {
+        if (args != null && args.length > 0) {
+            return Integer.parseInt(args[0]);
+        }
+
+        return DEFAULT_PORT;
+    }
+
+    private static boolean isConnected(ServerSocket serverSocket) {
+        return !serverSocket.isClosed();
     }
 }
